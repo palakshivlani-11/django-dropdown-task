601358605937f8aee1f6305c96c0e67e35e272a4
harsgdigi <harshkahuja@gmail.com>
2021-10-04 12:33:53 +0530

    first commint

--- ChainedDropdown/settings.py
+++ ChainedDropdown/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3xmh%hwy=aoip*6dui5j_)js$2613tem=+7t^$d50u&s%$7ob9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
